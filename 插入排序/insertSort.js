1aa167db40c696fef77a6307b89f001d20dd9fcf
twinMoons <gengxuepeng1991@163.com>
2020-06-06 23:04:24 +0800

    添加插入排序法

---
+++ 插入排序/insertSort.js
@@ -0,0 +1,15 @@
+function insetSort(arr){
+  let len = arr.length
+  for(let i=1; i<len; i++){
+    tem = arr[i]
+    while(i>0 && tem<arr[i-1]){
+      // 需要更换位置
+      arr[i] = arr[i-1]
+      i--
+    }
+    arr[i] = tem
+  }
+  return arr
+}
+let arr = [11, 3, 6, 5, 9, 1]
+console.log(insetSort(arr))
